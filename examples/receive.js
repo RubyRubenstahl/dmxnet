ac760fffa0b82e50a263f33d24c2bb51aafb841b
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-06-22 15:48:28 -0400

    Update

--- examples/receive.js
+++ examples/receive.js
@@ -9,13 +9,14 @@ var artnet = new Artnet({});
 // Create a new receiver instance, listening for universe 5 on net 0 subnet 0
 var receiver = artnet.createReceiver({
   subnet: 0,
-  universe: 1,
+  universe: 0,
   net: 0,
 });
 
-
+const toHex = val=> val.toString(16)
 
 // Dump data if DMX Data is received
-receiver.on('data', function(data) {
-  console.log('DMX data:', data); // eslint-disable-line no-console
+receiver.on('data', function (data) {
+  const formatted = data.map(toHex).slice(0,10).join(',')
+  console.log('DMX data:', formatted); // eslint-disable-line no-console
 });
